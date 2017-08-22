af856b6764d8147b8fa4fcec05bee69df518dc3b
Ilya Khaprov <dead.trickster@gmail.com>
2017-08-22 16:14:02 +0200

    sync mix project version with app version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Hooks.Mixfile do
 
   def project do
     [app: :hooks,
-     version: "1.2.0",
+     version: "2.0.1",
      description: "Generic plugin & hook system",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
